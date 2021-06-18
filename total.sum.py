e011aeb4728db1efa4e0b34397d26833dfb0d294
Amla63 <86015356+Amla63@users.noreply.github.com>
2021-06-19 00:40:19 +0530

    Add files via upload

---
+++ total.sum.py
@@ -0,0 +1,9 @@
+a=[2,3,4,5,6]
+i=0
+sum=0
+j=[]
+while i<len(a):
+	sum=sum+a[i]
+	j.append(sum)
+	i=i+1
+print(j)
---
+++ user.reverse.py
@@ -0,0 +1,7 @@
+list=['a',1,2,5,'b','q']
+user=int(input('enter the num'))
+i=-1
+a=len(list)
+while i<user:
+	print(list[i])
+	i=i-1
